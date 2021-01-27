8acfbcdbe6456c9cafe06ccc59609c626d06ba9d
ayushi <dummymail@gmail.com>
2021-01-27 11:57:07 +0000

    Change-password page added

--- url_new.tsx
+++ url_new.tsx
@@ -1,4 +1,4 @@
-const ngrok_ext = "11e6739ab6b4"
+const ngrok_ext = "210f8b1585d8"
 const is_google_colab = true
 export const deploy = is_google_colab ? "http://"+ngrok_ext+".ngrok.io" : "http://localhost:4000"
 console.log(deploy);
