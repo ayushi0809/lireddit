1c4942b80ff4473837af25966bc075b283b82d42
ayushi <dummymail@gmail.com>
2021-02-02 11:32:23 +0000

    forgot password became functional

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { Box, Flex, Link, Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import React, { useState } from 'react'
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/Wrapper';
+import { useForgotPasswordMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { toErrorMap } from '../utils/toErrorMap';
+import login from './login';
 
 export const ForgotPassword: React.FC<{}> = ({}) => {
-    return <div>forgot password</div>
+    const [complete,setComplete] = useState(false);
+    const [,forgotPassword]  = useForgotPasswordMutation()
+    return (<Wrapper variant = "small">
+    <Formik initialValues = {{ email:""}}
+    onSubmit={async (values) => {
+        await forgotPassword(values);
+        setComplete(true);
+
+            
+            
+    }}
+    >
+  
+        {({ isSubmitting }) => complete ? (<Box>if an account with that email exists, we sent you an email</Box>):(
+        <Form>
+          
+            <InputField name="email"
+              placeholder="email"
+              label="Email"
+              type="email">
+            </InputField>
+          <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">Forgot Password</Button>
+        </Form>
+
+      )}
+          </Formik>
+            </Wrapper>)
 };
 
-export default ForgotPassword;
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
--- url_new.tsx
+++ url_new.tsx
@@ -1,4 +1,4 @@
 const ngrok_ext = "210f8b1585d8"
 const is_google_colab = true
-export const deploy = is_google_colab ? "https://slippery-swan-7.loca.lt" : "http://localhost:4000"
+export const deploy = is_google_colab ? "https://gentle-newt-45.loca.lt" : "http://localhost:4000"
 console.log(deploy);
