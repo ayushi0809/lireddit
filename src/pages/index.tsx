bc17010ba2ca984ec3c9aede184bebe183e4a7e3
ayushi <dummymail@gmail.com>
2021-02-15 12:07:15 +0000

    NF: adding user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,7 @@ return (<Layout>
      {data!.posts.posts.map((p) => 
      <Box key = {p.id} p={5} shadow="md" borderWidth="1px">
      <Heading fontSize="xl">{p.title}</Heading>
+     <Text>Posted by {p.creator.username}</Text>
      <Text mt={4}>{p.textSnippet}</Text>
    </Box>
    )}
